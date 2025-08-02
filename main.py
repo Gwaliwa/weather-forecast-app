aadb84715971f031045a5a67c406b4ef8c530197
gmashaka <gwaliwa2010@gmail.com>
2025-08-02 17:04:04 -0400

    extract data from json

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+#import libraries
 import streamlit as st
+import plotly.express as px
+from backend import get_data
 
+#the interface widgets
 st.title("Weather Forecast App")
 place = st.text_input("Place")
 days = st.slider("Forecast Days", 1, 5, help="Number of days")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
+kind = get_data(place, days, option)
+
+#import graps functions
+figure = px.line(x=dates, y=kind, labels={'x':'Date', 'y':'Temperature'})
+st.plotly_chart(figure)
